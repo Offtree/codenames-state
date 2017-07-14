3b1654bd722141a1281870d3de5121fac2adeb3d
David Rowe <david.t.rowe@slalom.com>
2017-07-14 18:00:19 -0500

    fixed uiState

--- src/state/index.js
+++ src/state/index.js
@@ -3,7 +3,7 @@ import sendToReciverMiddleware from './sendToRecieverMiddleware';
 
 import boardReducer from './board/reducer';
 import gameReducer from './game/reducer';
-import uiStateReducer from './game/uiState';
+import uiStateReducer from './uiState/reducer';
 
 
 const reducers = combineReducers({
